7f4aabaf251de084360b38bd794151a2a6626ad8
Dayenne Souza <day.carol18@gmail.com>
2021-08-11 23:57:42 -0300

    initial commit

---
+++ src/components/pages/Home/Home.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import { memo } from "react";
+import styled from 'styled-components';
+
+export const Home:React.FC = memo(function Home() {
+return <Container>Home</Container>
+})
+
+const Container = styled.div``
